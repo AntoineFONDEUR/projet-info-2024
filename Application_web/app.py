6caaab958f8a0ad4ee2449bc6e630888d6b21a8e
Camille Prigent <camille.prigent@etu.minesparis.psl.eu>
2024-07-09 16:26:45 +0200

    modifs noémie

--- Application_web/app.py
+++ Application_web/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import csv
 from wordcloud import WordCloud
 import pandas as pd
+import numpy as np
 import io
 import base64
 from nltk.corpus import stopwords
@@ -45,7 +46,8 @@ def add_to_notion(data):
         "properties": {
             "Rep1": {"rich_text": [{"text": {"content": data['en_tant_que']}}]},
             "Rep2": {"rich_text": [{"text": {"content": data['jaimerais']}}]},
-            "Rep3": {"rich_text": [{"text": {"content": data['afin_de_parce_que']}}]}
+            "Rep3": {"rich_text": [{"text": {"content": data['afin_de_parce_que']}}]},
+            "Index": {"rich_text": [{"text": {"content": data['Index']}}]}
         }
     }
 
@@ -76,7 +78,8 @@ def fetch_data_from_notion():
         en_tant_que = properties['Rep1']['rich_text'][0]['text']['content']
         jaimerais = properties['Rep2']['rich_text'][0]['text']['content']
         afin_de_parce_que = properties['Rep3']['rich_text'][0]['text']['content']
-        rows.append([en_tant_que, jaimerais, afin_de_parce_que])
+        Index = properties['Index']['rich_text'][0]['text']['content']
+        rows.append([en_tant_que, jaimerais, afin_de_parce_que, Index])
     
     return rows
 
@@ -113,9 +116,11 @@ def fetch_data_from_notion_analyse():
         loisir = properties['loisir']['rich_text'][0]['text']['content']
         social = properties['social']['rich_text'][0]['text']['content']
         technologie = properties['technologie']['rich_text'][0]['text']['content']
-        num = properties['num']['rich_text'][0]['text']['content']
+        art = properties['art']['rich_text'][0]['text']['content']
+        emotion = properties['emotion']['rich_text'][0]['text']['content']
+        num = properties['num']['rich_text'][0]['text']['content'] #Indexs
 
-        rows.append([num,environnement, politique, agriculture, economie,education,transport,religion,sante,travail,sport,justice,loisir,social,technologie])
+        rows.append([num,environnement, politique, agriculture, economie,education,transport,religion,sante,travail,sport,justice,loisir,social,technologie, art, emotion])
     
     return rows
 
@@ -125,40 +130,81 @@ def home():
 
 @app.route('/resultats')
 def graphs():
-    # Récupérer les données depuis Notion
-    rows = fetch_data_from_notion_analyse()
+    import seaborn as sns
+    # Récupérer les Réponses triatées depuis Notion
+    reptraitees = fetch_data_from_notion_analyse()
 
-   
+    # Récupérer les Réponses triatées depuis Notion
+    repbrutes = fetch_data_from_notion()
 
     # Convertir les données en DataFrame
-    data = pd.DataFrame(rows, columns=['num','environnement', 'politique', 'agriculture', 'economie','education','transport','religion','sante','travail','sport','justice','loisir','social','technologie'])
+    datat = pd.DataFrame(reptraitees, columns=['num','environnement', 'politique', 'agriculture', 'economie','education','transport','religion','sante','travail','sport','justice','loisir','social','technologie', 'art','emotion'])
+    datatb = pd.DataFrame(repbrutes, columns=['en_tant_que', 'jaimerais', 'afin_de_parce_que', 'Index'])
+    
+    
+
 
     def find_major_themes(row):
         row = row.drop(labels=['num'])  # Ignorer la colonne 'num'
+        row = row.drop(labels=['emotion'])  # Ignorer la colonne 'emotion'
         sorted_themes = row.sort_values(ascending=False).index.tolist()
         return sorted_themes[0], sorted_themes[1]
 
-    data[['theme_major1', 'theme_major2']] = data.apply(find_major_themes, axis=1, result_type='expand')
+    datat[['theme_major1', 'theme_major2']] = datat.apply(find_major_themes, axis=1, result_type='expand')
 
-    # Afficher les trois premières lignes du DataFrame avec les nouvelles colonnes
-    print("Les trois premières lignes du DataFrame avec les thèmes majoritaires :")
-    print(data.head(3))
+
+    # Fusion en conservant tous les éléments du premier tableau
+    row = pd.merge(datat, datatb, left_on='num', right_on='Index', how='left')
+    row.drop(columns=['Index'], inplace=True)
+
+    row.to_csv('merged_data.csv', index=False)
+   
+
+
     # Crée des graphiques des thèmes les plus fréquents pour le premier et le deuxième thème majoritaire
-    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 10))
+    fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(21, 28))
 
     # Graphique pour le premier thème majoritaire
-    by_theme_major1 = data.groupby(by='theme_major1').size()
+    by_theme_major1 = datat.groupby(by='theme_major1').size()
     by_theme_major1.plot(kind='bar', ax=axes[0], color='blue')
     axes[0].set_title('Nombre de personnes par premier thème majoritaire')
     axes[0].set_xlabel('Thème')
     axes[0].set_ylabel('Nombre de personnes')
 
     # Graphique pour le deuxième thème majoritaire
-    by_theme_major2 = data.groupby(by='theme_major2').size()
+    by_theme_major2 = datat.groupby(by='theme_major2').size()
     by_theme_major2.plot(kind='bar', ax=axes[1], color='green')
     axes[1].set_title('Nombre de personnes par deuxième thème majoritaire')
     axes[1].set_xlabel('Thème')
     axes[1].set_ylabel('Nombre de personnes')
+
+    # Préparation des données pour le graphique à bulles
+    theme_counts = row.groupby(['en_tant_que', 'theme_major1']).size().reset_index(name='count')
+
+    # Définir une palette de couleurs pour les thèmes
+    palette = sns.color_palette('tab10', n_colors=len(datat['theme_major1'].unique()))
+
+    # Graphique à bulles pour la distribution des thèmes majoritaires par classe sociale
+    bubble_plot = sns.scatterplot(data=theme_counts, x='en_tant_que', y='theme_major1', size='count', hue='theme_major1', legend='full', palette=palette, sizes=(100, 2000), ax=axes[2])
+    axes[2].set_title('Distribution des Thèmes Majoritaires par Classe Sociale')
+    axes[2].set_xlabel('Classe Sociale')
+    axes[2].set_ylabel('Thème Majoritaire')
+    axes[2].tick_params(axis='x', rotation=45)
+
+    # Ajouter une légende pour la couleur des points à l'intérieur du graphique
+    #handles, labels = bubble_plot.get_legend_handles_labels()
+    #axes[2].legend(handles=handles, labels=labels, title='Thème Majoritaire', loc='center left', bbox_to_anchor=(1, 0.5), scatterpoints=1)
+
+    handles, labels = bubble_plot.get_legend_handles_labels()
+    axes[2].legend(handles=handles, labels=labels, title='Nombre de personnes', loc='center left', bbox_to_anchor=(1, 0.5), scatterpoints=1, fontsize='large')
+
+    emotions = row['emotion']
+    sorted_emotions = np.sort(emotions)
+    axes[3].hist(sorted_emotions, bins=10, edgecolor='black')  # Tracer l'histogramme avec 50 bins
+    axes[3].set_title('Histogramme des émotions')
+    axes[3].set_xlabel('Valeur des émotions')
+    axes[3].set_ylabel('Fréquence')
+    axes[3].grid(True)
 
     plt.tight_layout()
 
@@ -208,27 +254,52 @@ def save():
     jaimerais = request.form['jaimerais']
     afin_de_parce_que = request.form['afin_de_parce_que']
 
+    # Récupérer les données depuis Notion
+    rows = fetch_data_from_notion()
+
+
+    # Convertir les données en DataFrame avec toutes les colonnes nécessaires
+    df = pd.DataFrame(rows, columns=['Rep1', 'Rep2', 'Rep3', 'Index'])
+    if df['Index'].empty:
+        NewIndex = 1
+    else:
+        # Trouver le numéro le plus grand dans la colonne 'Index'
+        max_index = max(df['Index'].astype('int32'))
+        print(df['Index'].unique(),max_index)
+        # Convertir max_index en entier si ce n'est pas déjà le cas
+        max_index = int(max_index) if pd.notnull(max_index) else 0
+        # Générer un nouveau numéro qui sera +1 grand
+        NewIndex = max_index + 1
+    
+    Index = str(NewIndex)  # Convertir en chaîne de caractères
+
     # Sauvegarder dans un fichier CSV
     with open('data.csv', 'a', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow([en_tant_que, jaimerais, afin_de_parce_que])
 
     # Envoyer les données à Notion
-    notion_data = {'en_tant_que': en_tant_que, 'jaimerais': jaimerais, 'afin_de_parce_que': afin_de_parce_que}
+    notion_data = {
+        'en_tant_que': en_tant_que,
+        'jaimerais': jaimerais,
+        'afin_de_parce_que': afin_de_parce_que,
+        'Index': Index  # Assurez-vous que Index est une chaîne de caractères
+    }
     status = add_to_notion(notion_data)
 
     if status == 200:
         return redirect(url_for('generate_wordcloud'))
     else:
         return 'Réponse enregistrée mais échec de l\'envoi à Notion.'
-
+    
 @app.route('/generate_wordcloud')
 def generate_wordcloud():
     # Récupérer les données depuis Notion
     rows = fetch_data_from_notion()
 
     # Convertir les données en DataFrame
-    df = pd.DataFrame(rows, columns=['en_tant_que', 'jaimerais', 'afin_de_parce_que'])
+    df = pd.DataFrame(rows, columns=['en_tant_que', 'jaimerais', 'afin_de_parce_que', 'Index'])
+    df = df.drop(columns=['Index'])
 
     # Fonction pour filtrer les mots vides et les verbes
     def filter_words(sentence):
